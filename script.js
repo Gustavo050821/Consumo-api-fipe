4b46f705479193620199e429f42a97dc820fa569
Gustavo050821 <luisgustavofe2105@gmail.com>
2025-04-30 11:07:14 -0300

    flamengo virou codigo

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
     let id = document.querySelector('input').value
 
     if(id == ""){
-        alert("Digite um flamengo fipe")
+        alert("Digite um codigo fipe")
         return
     }
     
